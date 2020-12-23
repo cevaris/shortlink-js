9bc4e80dbbcb6e88b652f904f6a72cab88fd5fc7
Adam Cardenas <cevaris@gmail.com>
2020-12-23 14:13:27 -0700

    chose jest spy version

--- server/tests/expand.test.ts
+++ server/tests/expand.test.ts
@@ -2,11 +2,8 @@ import { afterAll, beforeAll, expect, test } from '@jest/globals';
 import http from 'http';
 import request from 'supertest';
 import { app } from '../src/app';
-import { linkDb, LinkDb } from '../src/storage/linkDb';
+import { LinkDb, linkDb } from '../src/storage/linkDb';
 import { Link } from '../src/types';
-
-jest.mock('../src/storage/linkDb');
-const mockedLinkDb = linkDb as jest.Mocked<LinkDb>;
 
 let server: http.Server;
 
@@ -19,14 +16,10 @@ afterAll(async (done) => {
 });
 
 test('existing /expand.json returns 200', async () => {
-    // const spy = jest.spyOn(linkDb, 'get');
-    // spy.mockReturnValue(Promise.resolve(link));
+    const link = new Link('link', 'http://link.com', new Date());
 
-    const now = new Date();
-    mockedLinkDb.get.mockImplementation((slug) => {
-        const link = new Link(slug, 'http://link.com', now);
-        return Promise.resolve(link)
-    });
+    const spy = jest.spyOn(linkDb, 'get');
+    spy.mockReturnValue(Promise.resolve(link));
 
     const resp = await request(server)
         .get('/expand.json?slug=TEST');
@@ -35,9 +28,9 @@ test('existing /expand.json returns 200', async () => {
     expect(resp.body).toStrictEqual({
         kind: 'link',
         data: {
-            slug: 'TEST',
-            link: 'http://link.com',
-            created_at: now.toISOString(),
+            slug: link.slug,
+            link: link.link,
+            created_at: link.createdAt.toISOString(),
         }
     })
 });
