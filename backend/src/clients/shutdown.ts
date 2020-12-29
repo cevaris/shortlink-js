a001ea3e3442368d0632bfe8da0c6bae44cdaf53
Adam Cardenas <cevaris@gmail.com>
2020-12-29 14:57:06 -0700

    updated shutdown signals

--- backend/src/clients/shutdown.ts
+++ backend/src/clients/shutdown.ts
@@ -2,27 +2,30 @@ import { Server } from "http";
 import { firebaseDb } from "./firebaseClient";
 import { pubSubClient } from "./pubsubClient";
 
+
+// https://gist.github.com/nfantone/1eaa803772025df69d07f4dbf5df7e58
 export function shutdownClientsOnExit(server: Server) {
-    async function close() {
+    function close() {
         console.log('About to exit, waiting for remaining connections to complete...');
-        server.close();
+        server.close(async () => {
+            try {
+                await pubSubClient.close();
+                console.error('pub/sub client closed');
+            } catch (error) {
+                console.error('could not close pub/sub client', error);
+            }
 
-        try {
-            await pubSubClient.close()
-            console.error('pub/sub client closed');
-        } catch (error) {
-            console.error('could not close pub/sub client');
-        }
-
-        try {
-            await firebaseDb.terminate();
-            console.error('firebase client closed');
-        } catch (error) {
-            console.error('could not close pub/sub client');
-        }
+            try {
+                await firebaseDb.terminate();
+                console.error('firebase client closed');
+            } catch (error) {
+                console.error('could not close firebase client', error);
+            }
+        });
     }
 
-    process.on('SIGINT', close);
-    process.on('SIGTERM', close);
+    process.on('SIGINT', close);  // CTRL+c
+    process.on('SIGTERM', close); // Node shutdown
+    process.on('SIGQUIT', close); // CTRL+\
     process.on('uncaughtException', close);
 }
