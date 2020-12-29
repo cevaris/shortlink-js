e5736626239d7c28121767a190a44a7d10eb8582
Adam Cardenas <cevaris@gmail.com>
2020-12-28 19:51:03 -0700

    cleanup

--- backend/src/http/responses.ts
+++ backend/src/http/responses.ts
@@ -1,23 +1,6 @@
 import express from 'express';
 import { ApiLink, ApiResponse } from '../api';
 
-// TODO: add ApiError type, update tests
-// export function apiError(res: express.Response, code: number, message: string) {
-//     res
-//         .status(code)
-//         .json({
-//             error: { code: code, message: message },
-//         });
-// }
-
-// export function apiErrors(res: express.Response, code: number, field: string, message: string) {
-//     res
-//         .status(code)
-//         .json({
-//             error: { code: code, errors: [{ [field]: message }] },
-//         });
-// }
-
 export function respond<T>(res: express.Response, apiResponse: ApiResponse<T>) {
     if (apiResponse.error) {
         return res
