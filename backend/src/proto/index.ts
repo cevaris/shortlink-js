1a5cf1eea8079e1e8ff7a101c2aad4e5da2e8339
Adam Cardenas <cevaris@gmail.com>
2020-12-30 10:12:39 -0700

    simplified proto export

--- backend/src/proto/index.ts
+++ backend/src/proto/index.ts
@@ -1,18 +1,2 @@
 import { com } from "./gen";
-
-// export type proto = com.company.links;
-export const proto = com.company.links;
-
-export module proto {
-    export type LinkCreateEvent = com.company.links.LinkCreateEvent;
-    export type LinkEvent = com.company.links.LinkEvent;
-    export type Link = com.company.links.Link;
-}
-
-// export type LinkCreateEvent = com.company.links.LinkCreateEvent;
-// export type LinkEvent = com.company.links.LinkEvent;
-// export type Link = com.company.links.Link;
-
-// export const LinkCreateEvent = com.company.links.LinkCreateEvent;
-// export const LinkEvent = com.company.links.LinkEvent;
-// export const Link = com.company.links.Link;
+export import proto = com.company.links;
