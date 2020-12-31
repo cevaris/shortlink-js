1cee4275c0819ba8a7c7d9bead96b617eb916c93
Adam Cardenas <cevaris@gmail.com>
2020-12-31 15:01:57 -0700

    screw _ah/start

--- backend/src/routes/gae.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-
-const router = express.Router();
-
-// Routes starting with /_ah/* are intentionally ignored express.
-// See https://stackoverflow.com/a/53606500/3538289
-router.get('/^(?!.*_ah).*$/', (req: express.Request, res: express.Response) => { });
-
-module.exports = router;
