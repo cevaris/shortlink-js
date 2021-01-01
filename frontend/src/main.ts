f3c3c503d0347b3b4eebfb9bb95d05a824583158
Adam Cardenas <cevaris@gmail.com>
2020-12-31 20:57:20 -0700

    cleanup

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -8,7 +8,5 @@ if (environment.production) {
   enableProdMode();
 }
 
-console.log('mode: ', environment.production);
-
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
