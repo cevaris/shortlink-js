50870eb7affebd0504419296671aa068e6140de9
Adam Cardenas <cevaris@gmail.com>
2021-01-18 13:04:02 -0700

    added some home tests

--- frontend/src/app/home/home.component.spec.ts
+++ frontend/src/app/home/home.component.spec.ts
@@ -2,7 +2,9 @@ import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { MatListModule } from '@angular/material/list';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
-import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
+import { By } from '@angular/platform-browser';
+import { ActivatedRoute } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
 import { InfiniteScrollModule } from 'ngx-infinite-scroll';
 import { HomeComponent } from './home.component';
@@ -10,26 +12,73 @@ import { HomeComponent } from './home.component';
 describe('HomeComponent', () => {
   let component: HomeComponent;
   let fixture: ComponentFixture<HomeComponent>;
+  let nativeElement;
+
+  let route: ActivatedRoute;
+  let spyRoute: jasmine.Spy<(name: string) => string>;
+
+  let snackBar: MatSnackBar;
+  let spySnackBar;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [HomeComponent],
       imports: [
-        HttpClientTestingModule,  
+        HttpClientTestingModule,
         InfiniteScrollModule,
         MatListModule,
         MatSnackBarModule,
         MatProgressSpinnerModule,
-        RouterTestingModule
+        RouterTestingModule.withRoutes([])
       ]
     }).compileComponents();
 
-    fixture = TestBed.createComponent(HomeComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    // https://stackoverflow.com/a/59160364/3538289
+    route = TestBed.inject(ActivatedRoute);
+    spyRoute = spyOn(route.snapshot.queryParamMap, 'get');
+
+    snackBar = TestBed.inject(MatSnackBar);
+    spySnackBar = spyOn(snackBar, 'open');
+
+    buildComponent();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should spin while loading', () => {
+    component.loading = true;
+    fixture.detectChanges();
+    expect(fixture.debugElement.query(By.css('mat-spinner'))).toBeTruthy();
+
+    component.loading = false;
+    fixture.detectChanges();
+    expect(fixture.debugElement.query(By.css('mat-spinner'))).toBeFalsy();
+  });
+
+  it('should render flash via snackbar', () => {
+    const flashMessage = 'test flash message';
+    spyRoute.and.returnValue(flashMessage);
+
+    // build after mocking
+    buildComponent();
+
+    expect(route.snapshot.queryParamMap.get).toHaveBeenCalledWith('flash');
+    expect(snackBar.open).toHaveBeenCalledWith(
+      flashMessage,
+      '',
+      {
+        verticalPosition: 'top', horizontalPosition: 'center',
+        panelClass: ['mat-toolbar', 'mat-accent']
+      }
+    )
+  });
+
+  function buildComponent() {
+    fixture = TestBed.createComponent(HomeComponent);
+    nativeElement = fixture.debugElement.nativeElement;
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  }
 });
